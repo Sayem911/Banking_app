196777c3e75f42754b656d9fb8c0b5139d828658
Sayem911 <sayemhossain911@gmail.com>
2024-07-04 00:20:39 +0600

    make Home layout and make route folder/files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import HeaderBox from "@/components/ui/HeaderBox";
+import RightSideBar from "@/components/ui/RightSideBar";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
+  const loggedIn = { firstName: "Sayem" , lastName: "Hossen" , email: "skbee911@gmail.com"};
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-hearder">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your acount and transaction efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT Transaction
+      </div>
+      <RightSideBar 
+      user = {loggedIn}
+      transactions = {[]}
+      banks = {[{currentBalance: 123.50},{currentBalance: 500}]}
+      />
+    </section>
+  );
 };
 
 export default Home;
